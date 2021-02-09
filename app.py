5f9c9272e2c7bf42f8334df4c1c17d59f21ecfb8
Hunter Hewitt <hunterchewitt@hotmail.com>
2021-02-09 00:08:13 -0600

    got gray working

---
+++ app.py
@@ -0,0 +1,67 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import cv2
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+    #paste link from fb?
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    to_sketch("uploads/" +filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+def to_sketch(img):
+    #gray_image = cv2.imread(img) #for color
+    gray_image = cv2.cvtColor(cv2.imread(img),cv2.COLOR_BGR2GRAY)
+    inverted_gray_image = 255 - gray_image
+    blurred_img = cv2.GaussianBlur(inverted_gray_image,(21,21),0)
+    inverted_blurred_img = 255 - blurred_img
+    pencil_sketch_IMG = cv2.divide(gray_image,inverted_blurred_img,scale=256.0)
+    cv2.imwrite(img, pencil_sketch_IMG)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+app.jinja_env.globals.update(to_sketch=to_sketch)
+
+if __name__ == '__main__':
+    app.run()
